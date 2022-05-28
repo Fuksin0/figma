9dd013e2cdedf165fb012537759fb7df9ad7d8c2
Konstantin Klimov <pumawc3@gmail.com>
2022-05-29 00:11:47 +0300

    new arrays.js

---
+++ git/arrays.js
@@ -0,0 +1,10 @@
+
+export const reverse = (massive) => {
+  const newMassive = [];
+  for (let i = massive.length - 1; i >= 0; i -= 1) {
+    const new = mass[i];
+Massive    newMassive.push(newMass + []);
+  }
+  console.log(newMassive);
+  return newMassive;
+}; 

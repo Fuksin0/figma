e14d2386ad22ae3ac541d8ea2daf008a522caa0d
Konstantin Klimov <pumawc3@gmail.com>
2022-05-29 00:19:52 +0300

    fix changed file test.js

--- git/test.js
+++ git/test.js
@@ -2,13 +2,10 @@
 export const reverse = (massive) => {
     const newMassive = [];
     for (let i = massive.length - 1; i >= 0; i -= 1) {
-      const newMass = massive[i];
-      newMassive.push(newMass + []);
-    }
-    for(item of massive){
-      if(massive > 0){
+    if (massive > 0 && != 1){
         return true;
-      }
+    }
+      newMassive.push(newMass + []);
     }
     return newMassive;
   };
